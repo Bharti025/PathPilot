d7abcf5a76898ee3c84d1bce1efeae0557cf429f
Bharti <110337668+Bharti025@users.noreply.github.com>
2026-02-26 14:24:38 +0000

    Pair done

---
+++ Pair.java
@@ -0,0 +1,11 @@
+public class Pair{
+int dist;
+int node;
+public Pair(int d,int n){
+    dist=d;
+    node=n;
+}
+public static void main(String[] args){
+    
+}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PathPilot
+This is a DSA based project which is used to find the shortest
+metro route by using Djikshtra algorithm.
